f6835e102206706dc34dc366b63965555b4fe10f
abubdcalling <abubdcalling@gmail.com>
2025-04-16 11:22:04 +0600

    frontend

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Achieve;
 use Illuminate\Http\Request;
 use App\Models\Banner;
+use App\Models\Body1;
+use App\Models\Body2;
 use App\Models\Feature;
 use App\Models\Footer;
 use App\Models\Header;
+use App\Models\Hero;
+use App\Models\MobileMockUp;
 
 class FrontendController extends Controller
 {
@@ -18,6 +23,12 @@ class FrontendController extends Controller
             'feature' => Feature::all(),
             'footer' => Footer::all(),
             'header' => Header::all(),
+            'mobile_mockup' => MobileMockUp::all(),
+            'achive'=>Achieve::all(),
+            'banner' => Banner::all(),
+            'hero'=>Hero::all(),
+            'body1'=>Body1::all(),
+            'body2'=>Body2::all(),
             
         ];
 
