c8d5440ed62894f15f92cf76f48d60be28e1b778
abubdcalling <abubdcalling@gmail.com>
2025-05-06 14:44:24 +0600

    add

--- app/Http/Controllers/UpdateController.php
+++ app/Http/Controllers/UpdateController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UpdateController extends Controller
 {
+
+    public function show(){
+        
+        return response()->json([
+            'email' => Auth::user()->email,
+            'password' => Auth::user()->password,
+            
+        ]);
+    }
+
     // Handle the update request
     public function update(Request $request)
     {
@@ -13,7 +26,7 @@ class UpdateController extends Controller
             'email' => 'required|email|unique:users,email,' . Auth::id(),
             'password' => 'required|string|min:8|confirmed',
         ]);
-
+        $user = User::find(Auth::id());
         $user = Auth::user();
         $user->email = $request->email;
         $user->password = Hash::make($request->password);
